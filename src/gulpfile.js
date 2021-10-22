1c3bf68a95a9105d428333ddc642d258700e0d7e
Parvaiz ZIAR <ziarparvaiz@gmail.com>
2021-10-22 19:37:50 +0300

    The infrastructure has been created, the homepage has been completed to a certain extent.

---
+++ src/gulpfile.js
@@ -0,0 +1,42 @@
+const { src, dest, watch, series } = require('gulp');
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const cssnano = require('cssnano');
+const terser = require('gulp-terser');
+const browsersync = require('browser-sync').create();
+
+// Sass Task
+function scssTask(){
+  return src('src/scss/style.scss', { sourcemaps: true })
+    .pipe(sass())
+    .pipe(postcss([cssnano()]))
+    .pipe(dest('./css', { sourcemaps: '.' }));
+}
+
+// Browsersync Tasks
+function browsersyncServe(cb){
+  browsersync.init({
+    server: {
+      baseDir: '../'
+    }
+  });
+  cb();
+}
+
+function browsersyncReload(cb){
+  browsersync.reload();
+  cb();
+}
+
+// Watch Task
+function watchTask(){
+  watch('*.html', browsersyncReload);
+  watch(['./scss/**/*.scss'], series(scssTask, browsersyncReload));
+}
+
+// Default Gulp task
+exports.default = series(
+  scssTask,
+  browsersyncServe,
+  watchTask
+);
---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+// ----------------------------------------------------
+
+// Home Page | .programming-language
+
+const swiper = new Swiper(".programming-language", {
+  slidesPerView: 5,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
+});
+
+// ----------------------------------------------------
