f940c8e5cc56efd8631856f42d90d11661f88467
Parvaiz ZIAR <ziarparvaiz@gmail.com>
2021-11-01 20:23:19 +0300

    Question part is finish

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,22 @@
 // ----------------------------------------------------
-
 // Home Page | .programming-language
 
-const swiper = new Swiper(".programming-language", {
+const swiper_programming_language = new Swiper(".programming-language", {
   slidesPerView: 5,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
+});
+
+// ----------------------------------------------------
+
+// ----------------------------------------------------
+// Home Page | .question-part
+
+const swiper_question_part = new Swiper(".question-part", {
+  slidesPerView: 3,
   spaceBetween: 30,
   pagination: {
     el: ".swiper-pagination",
