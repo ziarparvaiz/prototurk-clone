e91ae6eecf74dbfe6d6f6b923903ddacab6e054f
Parvaiz ZIAR <ziarparvaiz@gmail.com>
2022-10-05 15:09:59 +0300

    feat(ui): programmingLanguages component is done

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./Home.scss";
 import Header from "../../components/Header";
 import Sidebar from "../../components/Sidebar";
+import ProgrammingLanguages from "./components/ProgrammingLanguages";
 
 function Home() {
   return (
@@ -9,6 +10,17 @@ function Home() {
       <Header />
       <main>
         <Sidebar />
+        <div className="content">
+          <div className="content-inner">
+            <h3>Add value to yourself!</h3>
+            <p>
+              A good career requires the right start. You are ready to make a
+              right start with lessons on many languages and technologies at
+              Prototürk!
+            </p>
+            <ProgrammingLanguages />
+          </div>
+        </div>
       </main>
     </>
   );
